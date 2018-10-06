7638e05f544f71c2c8705e24fc68a8fcbac87425
YAlejandro Velazco <tim@timrich.org>
2018-10-05 21:26:05 -0400

    cambio22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -39,6 +40,7 @@ import { DireccionComponent } from './componentes/direccion/direccion.component'
     BrowserModule,
     NgbModule.forRoot(),
     AppRoutingModule,
+    TooltipModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
     AngularFirestoreModule
