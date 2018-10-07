9b711069dfcdc64cf47355fa5eb6a620c23471bc
YAlejandro Velazco <tim@timrich.org>
2018-10-07 13:34:04 -0400

    Ultimo cambios vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { PrincipalComponent } from './componentes/principal/principal.component';
@@ -21,6 +19,7 @@ import {AngularFirestoreModule, AngularFirestore} from 'angularfire2/firestore';
 import {environment} from '../environments/environment';
 import {AuthService} from './services/auth.service';
 import { DireccionComponent } from './componentes/direccion/direccion.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,17 +33,16 @@ import { DireccionComponent } from './componentes/direccion/direccion.component'
     EncabezadoComponent,
     MenuComponent,
     CambiarComponent,
-    DireccionComponent
+    DireccionComponent,
   ],
   imports: [
     BrowserModule,
-
     AppRoutingModule,
     TooltipModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
-    AngularFirestoreModule
-
+    AngularFirestoreModule,
+    FontAwesomeModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
